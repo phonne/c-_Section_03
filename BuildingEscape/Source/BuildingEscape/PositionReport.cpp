c669e22f5a227520578dc6e1e5882e3f41b303ac
HighwayZ <asino1990@live.com>
2017-07-19 22:33:56 +0100

    Lecture63

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -18,8 +18,8 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on Chair!"));
 
-	// ...
 	
 }
 
