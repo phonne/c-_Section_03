099c4e64de462b882d30786030592639dab2efdb
HighwayZ <asino1990@live.com>
2017-07-20 19:35:03 +0100

    Lecture68 UPROPERTY

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -22,7 +22,7 @@ void UOpenDoor::BeginPlay()
 	AActor* Owner = GetOwner();
 	
 	//Create a rorator
-	FRotator NewRotation = FRotator(0.f, -60.f, 0.f);
+	FRotator NewRotation = FRotator(0.f, -60.f, 0.f);     
 	
 	// Set the door rotation
 	Owner->SetActorRotation(NewRotation);
