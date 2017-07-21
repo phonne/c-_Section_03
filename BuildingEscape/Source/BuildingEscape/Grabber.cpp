337ea00a28585199f7c22906fb213fe9e22e2630
HighwayZ <asino1990@live.com>
2017-07-21 16:12:24 +0100

    Lecture79

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -49,8 +49,6 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	// drwa a red trace in the world to visualize
 	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
 
-
-	// ray-cast out to reach distance
 	DrawDebugLine(
 		GetWorld(),
 		PlayerViewPointLocation,
@@ -61,9 +59,25 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		0.f,10.f
 	);
 
+	// Setup query parameters
+	FCollisionQueryParams TraceParameters (FName(TEXT("")), false, GetOwner());
+
+	// Line-trace (AKA ray-cast) out to reach distance
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit, 
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
 
 	// see what we hit 
+	AActor* ActorHit = Hit.GetActor();
 
-	
+	if (ActorHit) 
+	{
+		UE_LOG(LogTemp,Warning,TEXT("Line trace hit: %s"), *ActorHit->GetName())
+	}	
 }
 
